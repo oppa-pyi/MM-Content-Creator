03bb5911b0fcfa0bb8011f9597e8a784c81b9473
Steven_Pyi <upyaephyoaungzx@gmail.com>
2026-05-18 10:00:27 +0630

    Update Gemini API model version in generate_gemini_text

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ PHONE_SHOP_TOPICS = [
 
 # ---------------- GEMINI FUNCTION ---------------- #
 def generate_gemini_text(prompt):
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite:generateContent?key={GEMINI_API_KEY}"
     
     headers = {"Content-Type": "application/json"}
     data = {
