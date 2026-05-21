fb243e56034e85b8f6d7b6adfc9aafad6ff17586
Steven_Pyi <upyaephyoaungzx@gmail.com>
2026-05-21 15:27:43 +0630

    Update app.py

--- app.py
+++ app.py
@@ -69,7 +69,7 @@ def get_topics_list():
 
 # ---------- GEMINI ----------
 def gemini_request(prompt):
-    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash:generateContent?key={GEMINI_API_KEY}"
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-3.1-flash-lite:generateContent?key={GEMINI_API_KEY}"
     data = {"contents": [{"parts": [{"text": prompt}]}]}
     for _ in range(2):
         try:
